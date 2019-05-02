122903817d98897ba9832b71ac960dc20d7a7b86
TrinhDuc997 <trinhxuanduc997@gmail.com>
2019-05-02 10:21:40 +0700

    Chinh Sửa trang index them dữ liêu hình ảnh vào database

---
+++ GlassesOnline/Controllers/DKDNController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GlassesOnline.Models;
+using System.Data.SqlClient;
+
+namespace GlassesOnline.Controllers
+{
+    public class DKDNController : Controller
+    {
+        DBGNamCuongEntities db = new DBGNamCuongEntities();
+        // GET: DKDN
+        public ActionResult ViewDangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection frmDN)
+        {
+            string sTaiKhoan = frmDN["TaiKhoan"];
+            string sMatKhau = frmDN["MatKhau"]; 
+            KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.TenDN == sTaiKhoan && n.MatKhau == sMatKhau);
+            //var kh1 = from KHACHHANG in db.KHACHHANGs
+            //          where KHACHHANG.TenDN == sTaiKhoan && KHACHHANG.MatKhau == sMatKhau
+            //          select KHACHHANG;
+            //int mkh = 0;
+            //foreach(var item in kh1)
+            //{
+            //    mkh = item.;
+            //}
+            if(kh != null)
+            {
+                Session["MaKH"] = kh.MaKH;
+                Session["TenKH"] = kh.HoTenKH;
+                return RedirectToAction("ViewIndex", "Index");
+            }
+            return RedirectToAction("ViewDangNhap", "DKDN");
+        }
+    }
+}
--- GlassesOnline/Controllers/IndexController.cs
+++ GlassesOnline/Controllers/IndexController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GlassesOnline.Models;
 
 namespace GlassesOnline.Controllers
 {
     public class IndexController : Controller
     {
+        DBGNamCuongEntities db = new DBGNamCuongEntities();
         // GET: Index
         public ActionResult ViewIndex()
-        {
-            return View();
+        {   
+            return View(db.SANPHAMGLAS.ToList());
         }
     }
 }
